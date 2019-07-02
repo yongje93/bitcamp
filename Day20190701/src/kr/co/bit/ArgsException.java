ba9a4f314da4134e61b8a4de7e1c9d6b14fa6eba
yongje93 <48149636+yongje93@users.noreply.github.com>
2019-07-02 10:13:56 +0900

    예외처리 테스트

--- Day20190701/src/kr/co/bit/ArgsException.java
+++ Day20190701/src/kr/co/bit/ArgsException.java
@@ -1,11 +1,13 @@
 package kr.co.bit;
 
+// 사용자정의 예외
 public class ArgsException extends Exception {
 	private int argsNumber;
-	
+
 	public ArgsException() {
 		super();
 	}
+
 	public ArgsException(String msg) {
 		super(msg);
 	}
--- Day20190701/src/kr/co/bit/ExceptionTest6.java
+++ Day20190701/src/kr/co/bit/ExceptionTest6.java
@@ -3,6 +3,12 @@ package kr.co.bit;
 public class ExceptionTest6 {
 	public void exceptionMethod() throws Exception {
 		throw new Exception();
+//		try {
+//			throw new Exception();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			System.out.println("메소드에서 예외처리");
+//		}
 	}
 	
 	public static void main(String[] args) {
@@ -10,7 +16,7 @@ public class ExceptionTest6 {
 		try {
 			et6.exceptionMethod();
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("호출한 메소드에서 예외처리함");
 		}
 	}
 }
