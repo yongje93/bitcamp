96403f8b7a0a2be970b364101120326bb86f9323
yongje93 <48149636+yongje93@users.noreply.github.com>
2019-07-13 20:03:21 +0900

    Update ServerEx02.java

--- Day20190710/src/kr/co/server/ServerEx02.java
+++ Day20190710/src/kr/co/server/ServerEx02.java
@@ -1,45 +1,34 @@
 package kr.co.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ServerEx {
-	public static void main(String[] args) {
+public class ServerEx02 {
+	
+	List<SocketThread> list = new ArrayList<SocketThread>();
+	
+	public ServerEx02() {
 		try {
-//			1.Serversocket 생성
 			ServerSocket server = new ServerSocket(10008);
-			System.out.println("Waiting Connection...");
-//			2.Serversocket의 accept() 메소드를 통해 클라이언트 접속을 대기
-//			클라이언트가 접속할 경우 accept() 메소드는 Socket을 리턴
-			Socket sock = server.accept();
-			InetAddress inetAddress = sock.getInetAddress();
-			System.out.println(inetAddress.getHostAddress()+" 로부터 접속했습니다.");
-//			3.반환받은 Socket으로부터 inputStream outputStream을 구현
-			OutputStream out = sock.getOutputStream();
-			InputStream in = sock.getInputStream();
-			PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			String line = null;
-//			4. readline 메소드를 이용해서 클라이언트가 보내는 문자열을 한줄 읽어들여 출력
-			while((line = br.readLine())!=null) {
-				System.out.println("클라이언트로부터 전달받은 문자열: "+line);
-//				5.클라이언트 쪽으로 문자열 전송
-				pw.println(":D");
-				pw.flush();
+			while(true) {
+				Socket sock = server.accept();
+				InetAddress inetaddr = sock.getInetAddress();
+				System.out.println(inetaddr.getHostAddress()+"�κ��� �����߽��ϴ�.");	
+				SocketThread st = new SocketThread(sock);
+				list.add(st);
+				st.start();
 			}
-			pw.close();
-			br.close();
-			sock.close();
-		} catch (Exception e) {
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public static void main(String[] args) {
+		new ServerEx02();
 	}
 }
