6e583719f66b0ad0fa8d7566f7f151901e5b580d
yongje93 <48149636+yongje93@users.noreply.github.com>
2019-07-24 00:09:35 +0900

    Update Number_3052.java

--- CodingTest/src/problem/Number_3052.java
+++ CodingTest/src/problem/Number_3052.java
@@ -1,7 +1,29 @@
 package problem;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Number_3052 {
 	public static void main(String[] args) {
-		
+//		Scanner scan = new Scanner(System.in);
+//		int[] rem = new int[10];
+//		int count = 1;
+//		
+//		for (int i = 0; i < 10; i++) {
+//			int x = scan.nextInt();
+//			rem[i] = x % 42;
+//		}
+//		Arrays.sort(rem);
+//		for (int i = 0; i < 9; i++) {
+//			if(rem[i]!=rem[i+1]) count++;
+//		}
+//		
+//		System.out.println(count);
+		Scanner sc = new Scanner(System.in);
+		Set<Integer> set = new HashSet<Integer>();
+		for (int i = 0; i < 10; i++)
+			set.add(sc.nextInt() % 42);
+		System.out.println(set.size());
 	}
 }
