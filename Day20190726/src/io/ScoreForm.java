a328ade29cf37c42e2f288958a80cb79fe1f7955
yongje93 <48149636+yongje93@users.noreply.github.com>
2019-07-29 17:47:21 +0900

    0729 init commit

--- Day20190726/src/io/ScoreForm.java
+++ Day20190726/src/io/ScoreForm.java
@@ -1,42 +1,51 @@
 package io;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 
-public class ScoreForm extends JFrame {
+public class ScoreForm extends JFrame implements ActionListener {
 	private JLabel hakL, nameL, korL, engL, mathL;
 	private JTextField hakT, nameT, korT, engT, mathT;
 	private JTextArea output;
 	private JButton addB, dispB, searchB, tot_descB, saveB, loadB; // tot_desc 총점으로 내림차순
+	private ScoreImpl impl;
 
 	public ScoreForm() {
-		hakL = new JLabel("학번  ", JLabel.RIGHT);
-		nameL = new JLabel("이름  ", JLabel.RIGHT);
-		korL = new JLabel("국어  ", JLabel.RIGHT);
-		engL = new JLabel("영어  ", JLabel.RIGHT);
-		mathL = new JLabel("수학  ", JLabel.RIGHT);
+		hakL = new JLabel("학번", JLabel.RIGHT);
+		nameL = new JLabel("이름", JLabel.RIGHT);
+		korL = new JLabel("국어", JLabel.RIGHT);
+		engL = new JLabel("영어", JLabel.RIGHT);
+		mathL = new JLabel("수학", JLabel.RIGHT);
 
-		hakT = new JTextField(11);
-		nameT = new JTextField(11);
-		korT = new JTextField(11);
-		engT = new JTextField(11);
-		mathT = new JTextField(11);
-				
-		output = new JTextArea(200, 200);
-		
+		hakT = new JTextField(15);
+		nameT = new JTextField(15);
+		korT = new JTextField(15);
+		engT = new JTextField(15);
+		mathT = new JTextField(15);
+
+		output = new JTextArea();
+		JScrollPane scroll = new JScrollPane(output);
+
+		impl = new ScoreImpl();
+
 		Border border = BorderFactory.createLineBorder(Color.GRAY);
-		output.setBorder(BorderFactory.createCompoundBorder(border,BorderFactory.createEmptyBorder(10, 10, 10, 10)));
-		//output.setBorder(border);
-		
+		output.setBorder(BorderFactory.createCompoundBorder(border, BorderFactory.createEmptyBorder(10, 10, 10, 10)));
+		output.setBorder(border);
+
 		addB = new JButton("입력");
 		dispB = new JButton("출력");
 		searchB = new JButton("학번검색");
@@ -50,24 +59,24 @@ public class ScoreForm extends JFrame {
 		tot_descB.setBackground(Color.YELLOW);
 		saveB.setBackground(Color.YELLOW);
 		loadB.setBackground(Color.YELLOW);
-		
+
 		JPanel panel = new JPanel(new GridLayout(1, 2, 10, 10));
 
 		JPanel left = new JPanel();
 
-		JPanel panel_1 = new JPanel(new GridLayout(1, 2));
+		JPanel panel_1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		panel_1.add(hakL);
 		panel_1.add(hakT);
-		JPanel panel_2 = new JPanel(new GridLayout(1, 2));
+		JPanel panel_2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		panel_2.add(nameL);
 		panel_2.add(nameT);
-		JPanel panel_3 = new JPanel(new GridLayout(1, 2));
+		JPanel panel_3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		panel_3.add(korL);
 		panel_3.add(korT);
-		JPanel panel_4 = new JPanel(new GridLayout(1, 2));
+		JPanel panel_4 = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		panel_4.add(engL);
 		panel_4.add(engT);
-		JPanel panel_5 = new JPanel(new GridLayout(1, 2));
+		JPanel panel_5 = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		panel_5.add(mathL);
 		panel_5.add(mathT);
 
@@ -78,7 +87,7 @@ public class ScoreForm extends JFrame {
 		left.add(panel_5);
 
 		panel.add(left);
-		panel.add(output);
+		panel.add(scroll);
 
 		JPanel downP = new JPanel(new GridLayout(1, 6));
 		downP.add(addB);
@@ -89,12 +98,59 @@ public class ScoreForm extends JFrame {
 		downP.add(loadB);
 
 		getContentPane().add("Center", panel);
-
 		getContentPane().add("South", downP);
 
 		setTitle("성적관리");
-		setBounds(200, 100, 550, 230);
+		setBounds(200, 100, 540, 240);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+
+	public void event() {
+		addB.addActionListener(this);
+		dispB.addActionListener(this);
+		searchB.addActionListener(this);
+		tot_descB.addActionListener(this);
+		saveB.addActionListener(this);
+		loadB.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == addB) {// 입력
+			// 데이터 얻어오기
+			String hak = hakT.getText();
+			String name = nameT.getText();
+			int kor = Integer.parseInt(korT.getText());
+			int eng = Integer.parseInt(engT.getText());
+			int math = Integer.parseInt(mathT.getText());
+
+			ScoreDTO dto = new ScoreDTO(hak, name, kor, eng, math);
+			dto.calc();
+
+			impl.input(dto);
+			JOptionPane.showMessageDialog(this, "등록완료!!");
+
+			hakT.setText("");
+			nameT.setText("");
+			korT.setText("");
+			engT.setText("");
+			mathT.setText("");
+
+		} else if (e.getSource() == dispB) {// 출력
+			impl.print(output);
+		} else if (e.getSource() == searchB) {// 학번검색
+			impl.search(output);
+		} else if (e.getSource() == tot_descB) {// 순위검색
+			impl.tot_desc();
+			impl.print(output);
+		} else if (e.getSource() == saveB) {// 파일저장
+			impl.save();
+		} else if (e.getSource() == loadB) {// 파일읽기
+			impl.load();
+			impl.print(output);
+		}
+
+	}
+
 }
--- Day20190726/src/io/ScoreMain.java
+++ Day20190726/src/io/ScoreMain.java
@@ -3,7 +3,7 @@ package io;
 public class ScoreMain {
 
 	public static void main(String[] args) {
-		new ScoreForm();
+		new ScoreForm().event();
 	}
 
 }
